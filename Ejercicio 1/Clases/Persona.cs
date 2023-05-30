4b68755b67febbe9091ad4402ecdff55517ea9a1
AdyGomezM <adyfabiolag@gmail.com>
2023-05-30 09:31:53 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Clases/Persona.cs
@@ -0,0 +1,54 @@
+﻿using Ejercicio_1.Funciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Clases
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set;}
+        public string Carrera { get; set; }
+        public int Edad { get; set;}
+
+        public void PedirDatos()
+        {
+            Console.WriteLine("Ingresa tu nombre");
+            this.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Apellido");
+            this.Apellido = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Carrera");
+            this.Carrera = Console.ReadLine();
+            Console.WriteLine("Ingresa tu Edad");
+            this.Edad = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            if (Edad >= 18)
+            {
+                Menu menu = new Menu();
+
+                menu.Opciones();
+                Console.WriteLine(this.Nombre);
+                Console.WriteLine(this.Apellido);
+                Console.WriteLine(this.Carrera);
+                Console.WriteLine(this.Edad);
+
+            }
+
+
+            else
+            {
+                Console.WriteLine("No puedes hacer las operaciones porque no cumples la mayoria de edad");
+            }
+        }
+
+        
+    }
+
+    
+}
+
---
+++ Ejercicio 1/Funciones/Menu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Funciones
+{
+    public class Menu
+    {
+        public int num1, num2, num3, num4,num5;
+        double dolarpeso;
+        public void Opciones()
+        {
+            Console.WriteLine("Ingresa una de las opciones");
+            Console.WriteLine("1: suma");
+            Console.WriteLine("2: resta");
+            Console.WriteLine("3: multiplicacion");
+            Console.WriteLine("4: Cambiar de dolar a pesos");
+            int opcion =int.Parse(Console.ReadLine());
+
+            Console.Clear();
+           
+
+
+            Operaciones operaciones = new Operaciones();
+            
+            double result  = 0;
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Ingresa el primer valor");
+                     num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el segundo valor");
+                     num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el tercer valor");
+                     num3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el cuarto valor");
+                     num4 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el quinto  valor");
+                     num5 = int.Parse(Console.ReadLine());
+                    result = operaciones.Suma(num1, num2, num3, num4, num5);
+                    Console.WriteLine("El resuldos es " + result);
+                    break;
+                case 2:
+                    Console.WriteLine("Ingresa el primer valor");
+                     num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el segundo valor");
+                    num2 = int.Parse(Console.ReadLine());
+                    result = operaciones.Resta(num1, num2);
+                    Console.WriteLine("El resultado es " + result);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Ingresa el primer valor");
+                    num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingresa el segundo valor");
+                     num2 = int.Parse(Console.ReadLine());
+                    result = operaciones.Multiplicacion(num1, num2);
+                    Console.WriteLine("El resultado es " + result);
+                    break;
+                case 4:
+                    Console.WriteLine("¿Cuantos dolares tienes ?");
+                    num1 = int.Parse(Console.ReadLine());
+                    result = operaciones.ValorDolar(num1, dolarpeso);
+                    Console.WriteLine("Tienes en pesos mexicanos : "+ result);
+                    break;
+                default:
+                    Console.WriteLine("No hay opcion; c");
+                    break;
+
+            }
+        }
+    }
+}
